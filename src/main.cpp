90b025aef1ae0daf40ad7cbb29b7fb6c3eed8686
IlReSenzaNome <alexisvivas3@gmail.com>
2023-10-07 18:48:50 -0500

    add reading list passwords and solucion problems

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
 #include <conio.h>
 #include <fstream>
+#include "lib/rlutil.h"
 
 int Option();
-std::string GeneratePassword(int);
+void GeneratePassword(int);
 void CheckFilePassword();
+void error();
 
 int main(int argc, char const *argv[])
 {
     Option();
-    getch();
     return 0;
 }
 
@@ -28,13 +31,24 @@ int Option()
         std::cout << "|> 4. Exit                   |" << std::endl;
         std::cout << "------------------------------" << std::endl;
         std::cout << "|> Select a option:          |" << std::endl;
+        rlutil::locate(21, 12);
         std::cin >> option;
         std::cout << "------------------------------" << std::endl;
-
         switch (option)
         {
         case 1:
-            GeneratePassword(large);
+            rlutil::cls();
+            std::cout << "Type a large for password: ";
+            std::cin >> large;
+            if (large > 0)
+            {
+                GeneratePassword(large);
+            }
+            else
+            {
+                error();
+                return 1;
+            }
             break;
         case 2:
             CheckFilePassword();
@@ -46,20 +60,67 @@ int Option()
             CheckFilePassword();
             break;
         default:
-            std::cout << "----------------------" << std::endl;
-            std::cout << "|       Error        |" << std::endl;
-            std::cout << "----------------------" << std::endl;
+            error();
             break;
         }
+        getch();
+        rlutil::cls();
     } while (option != 4);
 }
 
-std::string GeneratePassword(int large)
+void GeneratePassword(int large)
 {
-    std::cout << "end" << std::endl;
+    std::ofstream file;
+    char character[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=";
+    int numberofchar = sizeof(character) - 1, counter = 0;
+
+    // std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    std::srand(std::time(nullptr));
+
+    std::cout << "Password Generate: " << std::endl;
+    file.open("passwords.txt", std::ios::app);
+    counter++;
+    if (file.fail())
+    {
+        error();
+        exit(1);
+    }
+    else
+    {
+        file << counter << ".- ";
+        for (int i = 0; i < large; i++)
+        {
+            int randomindex = std::rand() % numberofchar;
+            char randomchracter = character[randomindex];
+            std::cout << randomchracter;
+            file << randomchracter;
+        }
+        file << std::endl;
+        file.close();
+        std::cout << std::endl;
+    }
 }
 
 void CheckFilePassword()
 {
-    std::cout << "test" << std::endl;
+    std::ifstream file;
+    std::string passwords;
+    file.open("passwords.txt", std::ios::in);
+    if (file.fail())
+    {
+        error();
+        exit(1);
+    }
+    while (!file.eof())
+    {
+        std::getline(file, passwords);
+        std::cout << passwords << std::endl;
+    }
+}
+
+void error()
+{
+    std::cout << "----------------------" << std::endl;
+    std::cout << "|       Error        |" << std::endl;
+    std::cout << "----------------------" << std::endl;
 }
